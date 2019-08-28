17538045e2618d3a4dd897fa00e719f4f3d05538
Aleksandr Pak <me@alekspak.com>
2019-08-28 06:32:21 +0200

    update to nightly

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,36 @@
 extern crate futures;
 extern crate telegram_bot;
-extern crate tokio_core;
+extern crate tokio;
 extern crate log;
 extern crate simple_logger;
 
 use std::env;
 
-use futures::Stream;
-use tokio_core::reactor::Core;
 use telegram_bot::*;
+use crate::tokio::prelude::*;
 use log::{info, Level};
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     simple_logger::init_with_level(Level::Info).unwrap();
 
-    let mut core = Core::new().unwrap();
+    let token = env::var("IGOR_TOKEN").expect("IGOR_TOKEN not set");
+    let api = Api::new(token);
 
-    let token = env::var("IGOR_TOKEN").unwrap();
-    let api = Api::configure(token).build(core.handle()).unwrap();
-
-    let future = api.stream().for_each(|update| {
+    let mut stream = api.stream();
+    while let Some(update) = stream.next().await {
+        let update = update?;
         if let UpdateKind::Message(message) = update.kind {
             if let MessageKind::Sticker {ref data} = message.kind {
                 if let Some(set_name) = &data.set_name {
                     if set_name == "igoryambro" {
                         info!("{:?} has sent sticker", message.from.username);
-                        api.spawn(message.delete())
+                        api.send(message.delete()).await?;
                     }
                 }
             }
         }
+    }
 
-        Ok(())
-    });
-
-    core.run(future).unwrap();
+    Ok(())
 }
